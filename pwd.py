8761f2bfbc70b852d891e73873557ff252f8f1b0
Clement MUGISHA <96583873+Bikaze@users.noreply.github.com>
2023-07-19 09:06:07 +0200

    Update pwd.py

--- pwd.py
+++ pwd.py
@@ -6,7 +6,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters= 10 #You can change the number of letter you want
+nr_letters= 10 #You can change the number of letters you want
 nr_symbols = 3 #You can change the number of symbols you want
 nr_numbers = 3 #You can change the number of numbers you want
 
